053fa2fc2edf29ab872be73b7bf119e77d19230e
KripsaKrips <144921464+TheMistaKrips@users.noreply.github.com>
2026-02-01 14:52:05 +0200

    main commit

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import image from './assets/illustration.png';
+
+function App() {
+
+  return (
+    <>
+      <div style={{
+        display: 'flex',
+        flexDirection: 'row'
+      }}>
+        {/* Левый див */}
+        <div>
+          <div>
+            <p style={{
+              color:'#263238',
+              fontWeight: 'bold'
+            }}>Lessons and insights</p>
+            <p style={{
+              color: '#28CB8B',
+              fontWeight: 'bold'
+            }}>from 8 years</p>
+          </div>
+          <div>
+            <button style={{
+              width: 100,
+              height: 50,
+              borderWidth: 2,
+              borderRadius: 10,
+              backgroundColor: '#28CB8B',
+              color: '#f3f3f3'
+            }}>
+                Register
+            </button>
+          </div>
+        </div>
+        {/* Правый див */}
+        <div>
+            <img src={image} style={{width: 200}}></img>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
