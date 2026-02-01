fe4e2521181c111d0397e8f235c535cda36cd9d5
KripsaKrips <144921464+TheMistaKrips@users.noreply.github.com>
2026-02-01 14:56:07 +0200

    edited

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import image from './assets/illustration.png';
+import image from '../public/illustration.png';
 
 function App() {
 
